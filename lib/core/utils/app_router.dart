7c803ddf8747f19b426dfb9b60526c424fcc95c7
Mahmoud Alshbly <shbly1029@gmail.com>
2026-03-04 14:43:01 -0800

    Refactor AppRouter to use CustomTransitionPage for smoother navigation transitions

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,28 +1,57 @@
 import 'package:car_parts_delivery_app/featrues/home/presentation/view/home_view.dart';
 import 'package:car_parts_delivery_app/featrues/notification/presentation/views/notification_view.dart';
 import 'package:car_parts_delivery_app/featrues/setting/presentation/view/setting_view.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
-   static const String kNotificationView = '/NotificationView';
-   static const String kSettingView = '/SettingView';
-
+  static const String kNotificationView = '/NotificationView';
+  static const String kSettingView = '/SettingView';
 
   static final router = GoRouter(
     routes: [
-      GoRoute(
+     GoRoute(
         path: '/',
-        builder: (context, state) => const HomeView(),
-      ),
-      GoRoute(
-        path: kNotificationView,
-        builder: (context, state) => const NotificationView(),
+        pageBuilder: (context, state) {
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: const HomeView(),
+            transitionDuration: Duration(milliseconds: 300),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+          );
+        },
       ),
       GoRoute(
         path: kSettingView,
-        builder: (context, state) => const SettingView(),
+        pageBuilder: (context, state) {
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: const SettingView(),
+            transitionDuration: Duration(milliseconds: 300),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+          );
+        },
+      ),
+      GoRoute(
+        path: kNotificationView,
+        pageBuilder: (context, state) {
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: const NotificationView(),
+            transitionDuration: Duration(milliseconds: 300),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+          );
+        },
       ),
     ],
   );
-
 }
